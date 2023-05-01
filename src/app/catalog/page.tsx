703ad44094778bdbe40f8769a7b1fa9da3f9e502
xuniorss <fortunatojunior2020@gmail.com>
2023-05-01 02:56:51 -0300

    deleting catalog

--- src/app/catalog/page.tsx
+++
@@ -1,9 +0,0 @@
-import CatalogTemplate from '@/templates/Catalog'
-
-export const metadata = {
-   title: 'Catálogo / Closet',
-}
-
-export default function Catalog() {
-   return <CatalogTemplate />
-}
